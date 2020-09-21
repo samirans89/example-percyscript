069fb6314c751c6df837e9d04e2fd4e7aae16e0b
Samiran Saha <samiran.s89@gmail.com>
2020-09-22 00:55:15 +0530

    latest changes

--- snapshots.js
+++ snapshots.js
@@ -25,16 +25,37 @@ PercyScript.run(async (page, percySnapshot) => {
 
  await percySnapshot('Porshe Login page', { enableJavaScript: true });
 
- await page.type('#username','<APPLICATION_USERNAME>');
+ await page.type('#username','<APP_USER>');
  await page.click('p-button[data-protractor-id="enter-password-btn"]');
-
- await page.type('#current-password','<APPLICATION_PASSWORD>');
+ await page.waitFor(2000);
+ await page.type('#current-password','<APP_PASS>');
 
  await page.click('p-button[type="submit"]');
+ await page.waitFor(8000);
 
- await page.waitFor('myprofile-address-list');
- await page.waitFor(5000);
- await percySnapshot('porsche personal data page');
+ await percySnapshot('Personal data page', { enableJavaScript: true });
+
+ await page.waitFor(8000);
+
+ await page.waitFor("a.navigation-items__item[href='https://preview-login.porsche.com/auth/de/de_DE/login-and-security']")
+ await page.click("a.navigation-items__item[href='https://preview-login.porsche.com/auth/de/de_DE/login-and-security']")
+ await page.waitFor(8000);
+ await percySnapshot('Login and Sec page', { enableJavaScript: true });
+
+ await page.waitFor("a.navigation-items__item[href='https://preview-profile.porsche.com/myprofile/de/de_DE/paymentmethods']")
+ await page.click("a.navigation-items__item[href='https://preview-profile.porsche.com/myprofile/de/de_DE/paymentmethods']")
+ await page.waitFor(8000);
+ await percySnapshot('paymentmethods page', { enableJavaScript: true });
+
+
+ await page.waitFor("a.navigation-items__item[href='https://preview-profile.porsche.com/myprofile/de/de_DE/dataprivacy']")
+ await page.click("a.navigation-items__item[href='https://preview-profile.porsche.com/myprofile/de/de_DE/dataprivacy']")
+ await page.waitFor(8000);
+ await percySnapshot('dataprivacy page', { enableJavaScript: true });
+
+ //await page.waitFor('myprofile-address-list');
+ //await page.waitFor(5000);
+ //await percySnapshot('porsche personal data page', { enableJavaScript: true });
 
   let server = httpServer.createServer();
   server.listen(PORT);
